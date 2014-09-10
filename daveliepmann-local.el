ea0df17333ab7ce0b388765ca2e4d0916a182f54
Dave Liepmann <dave.liepmann@gmail.com>
2014-09-11 00:11:52 +0300

    Fix Org-mode M-S-up/down

--- daveliepmann-local.el
+++ daveliepmann-local.el
@@ -1,43 +1,36 @@
-;; ;; Processing! via https://github.com/ptrv/processing2-emacs
-(setq processing-location "/usr/bin/processing-java")
-
-;; ;; Multiple cursors magic:
+;; Multiple cursors magic:
 (global-set-key (kbd "C-,") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-;; ; I don't want to type <yes> or <no> followed by <Enter>; when faced
-;; ; with such existential question I want to merely press <y> or <n>
-(fset 'yes-or-no-p 'y-or-n-p)
-
 ;;;; ORG-MODE ===============================================
-;; Let Shift-<arrow> select text instead of org-mode's custom
-;; functionality. This is a config option.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-support-shift-select t))
-(custom-set-faces)
-
-;; ;; Org-mode configuration, necessary for MobileOrg access.
-;; ;;  see http://mobileorg.ncogni.to/doc/getting-started/using-dropbox/
-;; ;;;;;;;;;;;;;; CURRENTLY NONFUNCTIONAL
-;; ;;;; Set to the location of your Org files on your local system
-;; (setq org-directory "~/Documents/Projects")
-;; ;;;; Set to the name of the file where new notes will be stored
-;; (setq org-mobile-inbox-for-pull "~/Documents/Projects/flagged.org")
-;; ;;;; Set to <your Dropbox root directory>/MobileOrg.
-;; (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 
 ;; Nobody puts in-file navigation in a corner.
 ;; Re-set keyboard navigation to up/down/sideways by logical unit.
 (require 'org)
 (eval-after-load 'org-mode
-  (dolist (binding (list (kbd "M-<up>") (kbd "M-<down>") (kbd "M-<left>") (kbd "M-<right>") (kbd "S-<up>") (kbd "S-<down>") (kbd "M-S-<left>") (kbd "M-S-<right>") (kbd "M-S-<down>") (kbd "M-S-<up>")))
+  (dolist (binding (list (kbd "M-<up>")
+                         (kbd "M-<down>")
+                         (kbd "M-<left>")
+                         (kbd "M-<right>")
+                         (kbd "S-<up>")
+                         (kbd "S-<down>")
+                         (kbd "C-S-<up>")
+                         (kbd "C-S-<down>")
+                         (kbd "C-<up>")
+                         (kbd "C-<down>")
+                         (kbd "M-S-<left>")
+                         (kbd "M-S-<right>")
+                         (kbd "M-S-<down>")
+                         (kbd "M-S-<up>")))
     (define-key org-mode-map binding nil)))
+
+(setq org-support-shift-select 'always)
+
+;; Unhand my M-S-up/down, you damn dirty apes
+(define-key org-mode-map [remap backward-paragraph] nil)
+(define-key org-mode-map [remap forward-paragraph] nil)
 
 ;;;; END ORG-MODE ===============================================
 
@@ -76,11 +69,14 @@
 ;; OS X Lion fullscreen mode command-return
 (global-set-key (kbd "<C-return>") 'toggle-fullscreen)
 
-;; Undo and redo window layout changes with C-c <left/right>
-(winner-mode 1)
+;; ;; Undo and redo window layout changes with C-c <left/right>
+;; (winner-mode 1)
 
 ;; Open .php files with html-mode
 (add-to-list 'auto-mode-alist '("\\.php\\'" . html-mode))
+
+;; Open .http files with REST-client mode
+(add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
 
 ;; Open .info files using the info command (Info-mode is insufficient)
 (defun info-mode ()
@@ -163,19 +159,9 @@ When there is a text selection, act on the region."
 (defun highlight-col-80 ()
   (interactive) (column-marker-1 80))
 
-;; ;; Highlight current line
-;; (global-hl-line-mode 1)
-;; ;; ...as unobtrusively as possible
-;; (set-face-background 'hl-line "#112211")
-;; ;; TODO doesn't play well with rainbow-mode. May be fixed in trunk 115615 of emacs.
-
 ;; I want diffs to show side-by-side
 (setq ediff-split-window-function 'split-window-horizontally
       ediff-window-setup-function 'ediff-setup-windows-plain)
-
-(require 'cider)
-
-(load "facts.el")
 
 ;;;; Python config
 ;; use IPython
@@ -200,16 +186,6 @@ When there is a text selection, act on the region."
             (font-lock-add-keywords
              nil '(("\\<\\(FIXME\\)" 1 font-lock-warning-face prepend)))))
 
-;; I always want to close CIDER. See cider-quit in
-;; cider-interaction.el for original w/y-or-n-p
-(defun really-quit-cider ()
-  (interactive)
-  (dolist (connection nrepl-connection-list)
-    (when connection
-      (nrepl-close connection)))
-  (message "All active nREPL connections were closed")
-  (cider-close-ancilliary-buffers))
-
 (defalias 'cider-quit 'really-quit-cider)
 
 ;; web-mode handles JS within HTML:
@@ -227,3 +203,19 @@ numbers where appropriate."
           (lambda () (local-set-key (kbd "C-c b") #'markdown-insert-bold)))
 (add-hook 'markdown-mode-hook
           (lambda () (local-set-key (kbd "C-c i") #'markdown-insert-italic)))
+
+;; ESS (Emacs Speaks Statistics) for R-mode
+(add-to-list 'load-path "/Applications/ess-13.09-1/lisp/")
+(load "ess-site")
+(require 'ess-site)
+(add-to-list 'auto-mode-alist '("\\.R$" . R-mode))
+
+;; When I add a numbered list item, automatically renumber the list.
+(defun markdown-insert-list-item-and-renumber (&optional arg)
+  (interactive "p")
+  (cond ((>= arg 0) (markdown-insert-list-item arg))
+        (markdown-insert-list-item))
+  (markdown-cleanup-list-numbers))
+
+(add-hook 'markdown-mode-hook
+          (lambda () (local-set-key (kbd "M-<return>") #'markdown-insert-list-item-and-renumber)))
